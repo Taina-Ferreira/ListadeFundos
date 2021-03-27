455416f8b8b1e62a1196d7cf2ad3bf153304520a
Tainá_Ferreira <tferreirarodrigues1@gmail.com>
2021-03-27 00:36:39 -0300

    Criando estilos globais - ThemeProvider

---
+++ src/Styles/globalstyles.js
@@ -0,0 +1,19 @@
+const green = "#639d31"
+const black = "#505152"
+const grey = "#9c9d9e"
+const white = "#fff"
+const bluechill = "#119c9f"
+
+const theme = {
+    colors: {
+        green,
+        black,
+        grey,
+        white,
+        bluechill
+    },
+}
+
+
+
+export default theme;
